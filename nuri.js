9dc951f0b69d75a313d18480de908325bdb8f8e4
Nuri <nuri1999sept@gmail.com>
2022-12-27 20:22:16 +0600

    final push

---
+++ nuri.js
@@ -0,0 +1,54 @@
+/*var nuri,ex,ex1,ex2,ex3,ex4,ex5;
+nuri=10;
+function print(demo){
+    console.log(demo);
+}
+ex=[
+    "nuri",
+    2,3,4,"jj",
+]
+ex1={
+    name:'nuri',
+    id:'237',
+    call:88
+}
+print(ex)
+print(ex1) */
+
+
+
+//in html file script write 
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <title>
+       basic syntext
+    </title>
+</head>
+ 
+<body>
+ 
+    <script>
+        console.log("Ishmoth ura nuri ");
+        var Name = "Apple";
+  
+    // Function definition
+    function MyFunction() {
+  
+        // Local variable declaration
+        var num = 237;
+  
+        // Console value of Global variable
+        console.log(Name);
+  
+        // Console value of local variable
+        console.log("\n" + num);
+    }
+      
+    // Function call
+    MyFunction();
+    </script>
+</body>
+ 
+</html>
